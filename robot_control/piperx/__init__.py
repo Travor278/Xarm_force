07b7c50f7c304a11953b1663e8412f51774c7c5a
Travor <3488616445@qq.com>
2026-08-11 11:30:37 +0800

    feat: export PiperX estimator library API

--- robot_control/piperx/__init__.py
+++ robot_control/piperx/__init__.py
@@ -1,2 +1,23 @@
 """Receive-only PiperX external joint-torque estimation."""
 
+from .calibration import CalibrationArtifact, fit_calibration
+from .dynamics import PinocchioDynamics
+from .estimator import ExternalTorqueEstimator
+from .filtering import VelocityDerivativeFilter
+from .records import TorqueLog, load_torque_log, save_torque_log
+from .socketcan import PiperStateAssembler, ReadOnlySocketCan, discover_interface
+
+
+__all__ = [
+    "CalibrationArtifact",
+    "ExternalTorqueEstimator",
+    "PinocchioDynamics",
+    "PiperStateAssembler",
+    "ReadOnlySocketCan",
+    "TorqueLog",
+    "VelocityDerivativeFilter",
+    "discover_interface",
+    "fit_calibration",
+    "load_torque_log",
+    "save_torque_log",
+]
---
+++ tests/piperx/test_public_api.py
@@ -0,0 +1,20 @@
+import robot_control.piperx as piperx
+
+
+def test_package_exports_supported_library_entrypoints():
+    expected = {
+        "CalibrationArtifact",
+        "ExternalTorqueEstimator",
+        "PinocchioDynamics",
+        "PiperStateAssembler",
+        "ReadOnlySocketCan",
+        "TorqueLog",
+        "VelocityDerivativeFilter",
+        "discover_interface",
+        "fit_calibration",
+        "load_torque_log",
+        "save_torque_log",
+    }
+
+    assert set(piperx.__all__) == expected
+    assert all(hasattr(piperx, name) for name in expected)
